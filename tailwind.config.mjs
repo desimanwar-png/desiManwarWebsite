f3e3d1e86e1d7f6766f3bbce4d3a36a037e1c394
Ashutosh Sonel <ashutoshhoonmain@gmail.com>
2024-12-14 19:00:40 +0530

    Initial Commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const tailwindConfig = {
+  // darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          base: '#ffedff',
+          dark: '#6c0089',
+        },
+        secondary: {
+          base: '#b200e3',
+          dark: '#24002e',
+        },
+        accent: {
+          base: '#91e600',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require('tailwindcss-animate')],
+}
+
+export default tailwindConfig
