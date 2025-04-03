48d5c32b09d672f543dc261d6bc63727ea859482
Ashutosh Sonel <ashutoshhoonmain@gmail.com>
2025-04-03 21:04:03 +0530

    Updated the frontend to fetch members from DB

--- src/app/client/home/OurMembers.js
+++ src/app/client/home/OurMembers.js
@@ -1,28 +1,23 @@
+import { getMembers } from '@/app/admin/members/actions'
 import MemberCard from '@/components/MemberCard'
-import React from 'react'
+import React, { Suspense } from 'react'
 
-const members = [
-  {
-    _id: '1',
-    name: 'John Doe',
-    role: 'Event Organizer',
-    image: '',
-  },
-  {
-    _id: '2',
-    name: 'Jane Smith',
-    role: 'Community Outreach',
-    image: '',
-  },
-  {
-    _id: '3',
-    name: 'Mike Brown',
-    role: 'Volunteer Coordinator',
-    image: '',
-  },
-]
+async function fetchSortedMembers() {
+  try {
+    const response = await getMembers()
+    if (response.status === 'success') {
+      return response.data.sort((a, b) => a.priority - b.priority)
+    }
+    return []
+  } catch (error) {
+    console.error('Error fetching members:', error)
+    return []
+  }
+}
 
-function OurMembers() {
+async function OurMembers() {
+  const members = await fetchSortedMembers()
+
   return (
     <div className="px-4 lg:px-20 pt-12 lg:pt-6">
       <div className="py-20">
@@ -30,11 +25,25 @@ function OurMembers() {
           _ Meet Our
           <span className="text-accent-base pl-2 lg:pl-4"> Members _</span>
         </h1>
-        <div className="pt-16 pb-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {members.map((member) => (
-            <MemberCard key={member._id} member={member} />
-          ))}
-        </div>
+
+        <Suspense
+          fallback={
+            <p className="text-center text-gray-500">Loading members...</p>
+          }
+        >
+          <div className="pt-16 pb-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {members.length > 0 ? (
+              members.map((member) => (
+                <MemberCard key={member._id} member={member} />
+              ))
+            ) : (
+              <p className="text-center text-gray-500 col-span-3">
+                No members found
+              </p>
+            )}
+          </div>
+        </Suspense>
+
         <div className="flex justify-end text-secondary-dark dark:text-secondary-base hover:text-accent-base font-semibold tracking-wide">
           <h1 className="hover:scale-105 transition-all ease-in-out text-xl cursor-pointer mx-2">
             Show More
